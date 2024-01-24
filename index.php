139849ef8096cab6fa6e569b35eb51530b4bbd6b
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-01-24 14:31:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+   include('db.php')
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Form Login and Signup</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+          <div class="main">
+            <div class="navbar">
+              <div class="icon">
+                
+</div>
+
+<div class="menu">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Service</a></li>
+    <li><a href="#">Contact</a></li>
+</ul>
+</div>
+
+<div class="login-signup">
+  <button><a href="login.php">LogIn</a></button>
+</div>
+</div>
+</div>
+
+</body>
+</html>
